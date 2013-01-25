2e812714cc87097fb19fd08e67529ba21d497a02
Nithin Reddy <nithin@ubuntu.(none)>
2013-01-24 17:01:46 -0800

    Initial commit of vim directory.

---
+++ get_plugins.bash
@@ -0,0 +1,12 @@
+git clone https://github.com/mileszs/ack.vim.git bundle/ack
+git clone https://github.com/kien/ctrlp.vim bundle/ctrlp.vim
+git clone https://github.com/sjl/gundo.vim.git bundle/gundo
+git clone https://github.com/scrooloose/nerdtree bundle/nerdtree
+git clone https://github.com/vim-scripts/pep8.git bundle/pep8
+git clone https://github.com/fs111/pydoc.vim.git bundle/pydoc
+git clone https://github.com/mitechie/pyflakes-pathogen.git
+git clone https://github.com/msanders/snipmate.vim.git bundle/snipmate
+git clone https://github.com/ervandew/supertab.git bundle/supertab
+git clone https://github.com/altercation/vim-colors-solarized bundle/vim-colors-solarized
+git clone https://github.com/tpope/vim-markdown bundle/vim-markdown
+git clone https://github.com/tpope/vim-surround.git bundle/surround
