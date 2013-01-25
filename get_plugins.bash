815613024f14d79a9ec50bd3b5ce27d3719dc59e
Nithin Reddy <jashugan@gmail.com>
2013-01-24 17:24:33 -0800

    Updated locations and .gitignore.

--- get_plugins.bash
+++ get_plugins.bash
@@ -4,7 +4,7 @@ git clone https://github.com/sjl/gundo.vim.git bundle/gundo
 git clone https://github.com/scrooloose/nerdtree bundle/nerdtree
 git clone https://github.com/vim-scripts/pep8.git bundle/pep8
 git clone https://github.com/fs111/pydoc.vim.git bundle/pydoc
-git clone https://github.com/mitechie/pyflakes-pathogen.git
+git clone https://github.com/mitechie/pyflakes-pathogen.git bundle/pyflakes-pathogen.git
 git clone https://github.com/msanders/snipmate.vim.git bundle/snipmate
 git clone https://github.com/ervandew/supertab.git bundle/supertab
 git clone https://github.com/altercation/vim-colors-solarized bundle/vim-colors-solarized
